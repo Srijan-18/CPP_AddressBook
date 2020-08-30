7a86114376bcfad511d20e14c9c4171c7f92f85c
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-30 11:22:10 +0530

    Welcome message.

---
+++ AddressBookMain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void presentWelcomeMessage(){
+    cout << "\n\t\t\t**** WELCOME TO ADDRESS BOOK PROGRAM ****" << endl;
+}
+int main()
+{
+    presentWelcomeMessage();
+}
