0f1034850d999655c38a3cb1001bba6868c657d8
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-30 16:44:05 +0530

    ADDED: Ability to take input from user for a person's detail and display it at console.

--- AddressBookMain.cpp
+++ AddressBookMain.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
+#include "Person.cpp"
+#include "InputUtility.cpp"
 
 using namespace std;
 
-void presentWelcomeMessage(){
+void presentWelcomeMessage()
+{
     cout << "\n\t\t\t**** WELCOME TO ADDRESS BOOK PROGRAM ****" << endl;
 }
+
+Person getPersonFromUser()
+{
+    InputUtility inputUtility;
+    
+    string firstName = inputUtility.getStringInputfor("First Name");
+    string lastName = inputUtility.getStringInputfor("Last Name");
+    string address = inputUtility.getStringInputfor("Address");
+    string city = inputUtility.getStringInputfor("City");
+    string state = inputUtility.getStringInputfor("State");
+    string phoneNumber = inputUtility.getStringInputfor("Phone Number");
+    int zip;
+    cout << "\nEnter Zipcode : ";
+    cin >> zip;
+
+    Person person(firstName, lastName, address, city, state, zip, phoneNumber);
+
+    return person;
+}
+
+void addPerson()
+{
+    Person person = getPersonFromUser();
+
+    person.printDetails();
+}
+
 int main()
 {
     presentWelcomeMessage();
-
+    addPerson();
     return 0;
 }
